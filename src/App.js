1872392f30f52b4c53cc17b5edb9ea3d39760a22
Camila <camila.chavez@pucp.pe>
2019-09-11 23:05:13 -0500

    Homepage layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
   return (
-    <div>Hola albuferas</div>
+    <div>
+      <HomePage  />
+    </div>
   );
 }
 
