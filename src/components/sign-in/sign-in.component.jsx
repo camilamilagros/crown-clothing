2beeb0df545707a766b6027a2e004619c3f0aa54
Camila <camila.chavez@pucp.pe>
2019-09-16 22:57:46 -0500

    Sign in styles

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
 
@@ -48,7 +51,9 @@ class SignIn extends React.Component {
                         required 
                     />
 
-                    <input type="submit" value="Submit Form"/>
+                    <CustomButton type="submit">
+                        SIGN IN
+                    </CustomButton>
                 </form>
             </div>
         )
