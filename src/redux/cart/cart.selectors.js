19a2998c678dd9df1d11f73dca1d522434b3685f
Camila <camila.chavez@pucp.pe>
2019-10-01 22:30:44 -0500

    Add cart selectors to cartItems and ItemsCount

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,14 @@
+import { createSelector } from 'reselect';
+
+// input selectors
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((acc, item) => (acc + item.quantity), 0)
+);
