e11c35dbd7b5061d4e3a89c60c863109eb21b4d0
Camila <camila.chavez@pucp.pe>
2019-11-25 22:12:24 -0500

    Collection routes

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+import { selectCollection } from '../../redux/shop/shop.selector';
+import './collection.styles.scss';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection, match }) => {
+    const { title, items } = collection;
+
+    return (
+        <div className='collection-page'>
+            <h2 className='title'>{ title }</h2>
+            <div className="items">
+                {
+                    items.map(item => <CollectionItem key={item.id} item={item}/>)
+                }
+            </div>
+        </div>
+    )
+};
+
+const mapStateToProps = (state, props) => ({
+    collection: selectCollection(props.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
