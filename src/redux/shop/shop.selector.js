30695324fa89653c68ed8473f0dc3066c9182e3c
Camila <camila.chavez@pucp.pe>
2019-11-26 23:10:31 -0500

    Stripe setup c:

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -15,9 +7,12 @@ export const selectCollections = createSelector(
     shop => shop.collections
 )
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.values(collections)
+)
+
 export const selectCollection = collectionUrlId => createSelector(
     [selectCollections],
-    collections => collections.find(
-        collection => collection.id === COLLECTION_ID_MAP[collectionUrlId]
-    )
+    collections => collections[collectionUrlId]
 );
